c16cea0fdafa2e64dad53d7cd08da63182778e29
Milos Petrovic <87143160+Rebbul@users.noreply.github.com>
2023-08-26 23:09:58 +0200

    amiri

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import images from '../../images/images'
 
 import './footer.css'
 export default function Footer() {
   return (
     <div className='footer'>
-      <img 
-        src='https://cdn.shopify.com/s/files/1/1056/1394/files/monogram_efda229b-3c71-4da6-b40c-1fac6bd4089f.png?v=1678302624&width=360' />
+      <img src={images["./monogram.webp"]} />
     </div>
   )
 }
